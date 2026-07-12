842c23fbe940f15b0d7577d2ad4979ec9df2c2cc
cadicedit2k5@gmail.com <cadicedit2k5@gmail.com>
2026-07-12 22:20:28 +0700

    feat: Add BaseEntity Class to define model

---
+++ backend/common/src/main/java/com/smartlearning/common/entity/BaseEntity.java
@@ -0,0 +1,35 @@
+package com.smartlearning.common.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    protected LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    protected LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void prePersist() {
+        LocalDateTime now = LocalDateTime.now();
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    @PreUpdate
+    protected void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
--- backend/system/src/main/java/com/smartlearning/system/user/entity/User.java
+++ backend/system/src/main/java/com/smartlearning/system/user/entity/User.java
@@ -1,20 +1,15 @@
 package com.smartlearning.system.user.entity;
 
+import com.smartlearning.common.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "user", schema = "system")
 @Getter
 @Setter
-public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
+public class User extends BaseEntity {
     @Column(nullable = false, unique = true, length = 100)
     private String email;
 
@@ -30,24 +25,4 @@ public class User {
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "role_id")
     private Role role;
-
-    @Column(name = "created_at", updatable = false)
-    private LocalDateTime createdAt;
-
-    @Column(name = "updated_at")
-    private LocalDateTime updatedAt;
-
-    @PrePersist
-    public void prePersist() {
-        LocalDateTime now = LocalDateTime.now();
-        this.createdAt = now;
-        this.updatedAt = now;
-        this.email = this.email.trim().toLowerCase();
-    }
-
-    @PreUpdate
-    public void preUpdate() {
-        this.updatedAt = LocalDateTime.now();
-        this.email = this.email.trim().toLowerCase();
-    }
 }
