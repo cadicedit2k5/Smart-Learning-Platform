a9419c71e9c093df8cce00cbd3623764078d4a36
cadicedit2k5@gmail.com <cadicedit2k5@gmail.com>
2026-07-12 20:38:12 +0700

    feat(auth): Implement data initialization for roles and admin user in the database

---
+++ backend/system/src/main/resources/data.sql
@@ -0,0 +1,20 @@
+INSERT INTO system.roles (code, name)
+VALUES
+    ('ADMIN', 'Administrator'),
+    ('STUDENT', 'Student'),
+    ('LECTURER', 'Lecturer')
+ON CONFLICT (code) DO NOTHING;
+
+INSERT INTO system."user"
+    (email, password, full_name, avatar, role_id, created_at, updated_at)
+SELECT
+    'admin@gmail.com',
+    '$2a$10$geQo4kEjV22SLPDoKb80YOWdj8csdZ/qEPSoV2KsFVyX7DEEuc80i',
+    'Administrator',
+    NULL,
+    role.id,
+    CURRENT_TIMESTAMP,
+    CURRENT_TIMESTAMP
+FROM system.roles role
+WHERE role.code = 'ADMIN'
+ON CONFLICT (email) DO NOTHING;
