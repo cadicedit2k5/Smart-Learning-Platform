7cd3cb0ef9b34e4bebef759a62aa0a8f623f31a1
cadicedit2k5@gmail.com <cadicedit2k5@gmail.com>
2026-08-23 16:32:13 +0700

    chore: initialize ai module

---
+++ backend/ai/src/main/java/com/smartlearning/ai/AiApplication.java
@@ -0,0 +1,18 @@
+package com.smartlearning.ai;
+
+import com.smartlearning.common.config.CommonSercurityConfigs;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+@Import({
+        CommonSercurityConfigs.class,
+})
+public class AiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiApplication.class, args);
+    }
+
+}
